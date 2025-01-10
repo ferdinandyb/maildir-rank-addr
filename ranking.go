6cba1ce0350fea9b003ad70432f1036530e5e7cb
Bence Ferdinandy <bence@ferdinandy.com>
2025-01-10 23:33:55 +0100

    ranking: refactor for readability and testability

--- ranking.go
+++ ranking.go
@@ -9,6 +9,11 @@ import (
 	"golang.org/x/text/unicode/norm"
 )
 
+type KeyValue struct {
+	normaddr string
+	addrdata AddressData
+}
+
 func getMostFrequent(names []string) string {
 	counter := make(map[string]int)
 	for _, name := range names {
@@ -50,11 +55,55 @@ func normalizeAddressNames(aD AddressData) AddressData {
 	return aD
 }
 
-func calculateRanks(data map[string]AddressData) map[int]map[string]AddressData {
-	type KeyValue struct {
-		normaddr string
-		addrdata AddressData
+func sortByFrequency(s []KeyValue, class int) {
+	sort.SliceStable(s, func(i, j int) bool {
+		if s[i].addrdata.ClassCount[class] == s[j].addrdata.ClassCount[class] {
+			return s[i].addrdata.Address < s[j].addrdata.Address
+		} else {
+			return s[i].addrdata.ClassCount[class] > s[j].addrdata.ClassCount[class]
+		}
+	})
+}
+
+func sortByRecency(s []KeyValue, class int) {
+	sort.SliceStable(s, func(i, j int) bool {
+		if s[i].addrdata.ClassDate[class] == s[j].addrdata.ClassDate[class] {
+			return s[i].addrdata.Address < s[j].addrdata.Address
+		} else {
+			return s[i].addrdata.ClassDate[class] > s[j].addrdata.ClassDate[class]
+		}
+	})
+}
+
+func getClassRanks(addrmap map[string]AddressData, class int) map[string]AddressData {
+	s := make([]KeyValue, 0, len(addrmap))
+	for normaddr, addrdata := range addrmap {
+		s = append(s, KeyValue{normaddr, addrdata})
 	}
+
+	sortByFrequency(s, class)
+	for rank, kv := range s {
+		addrdata, _ := addrmap[kv.normaddr]
+		addrdata.FrequencyRank = rank
+		addrmap[kv.normaddr] = addrdata
+	}
+
+	sortByRecency(s, class)
+	for rank, kv := range s {
+		addrdata, _ := addrmap[kv.normaddr]
+		addrdata.RecencyRank = rank
+		addrmap[kv.normaddr] = addrdata
+	}
+
+	for normaddr, addrdata := range addrmap {
+		addrdata.TotalRank = addrdata.FrequencyRank + addrdata.RecencyRank
+		addrmap[normaddr] = addrdata
+	}
+
+	return addrmap
+}
+
+func calculateRanks(data map[string]AddressData) map[int]map[string]AddressData {
 	classedData := map[int]map[string]AddressData{
 		2: {},
 		1: {},
@@ -65,41 +114,7 @@ func calculateRanks(data map[string]AddressData) map[int]map[string]AddressData 
 	}
 
 	for class := 2; class >= 0; class-- {
-		thisclass, _ := classedData[class]
-		s := make([]KeyValue, 0, len(thisclass))
-		for normaddr, addrdata := range thisclass {
-			s = append(s, KeyValue{normaddr, addrdata})
-		}
-		sort.SliceStable(s, func(i, j int) bool {
-			if s[i].addrdata.ClassCount[class] == s[j].addrdata.ClassCount[class] {
-				return s[i].addrdata.Address < s[j].addrdata.Address
-			} else {
-				return s[i].addrdata.ClassCount[class] > s[j].addrdata.ClassCount[class]
-			}
-		})
-		for rank, kv := range s {
-			thisval, _ := thisclass[kv.normaddr]
-			thisval.FrequencyRank = rank
-			thisclass[kv.normaddr] = thisval
-		}
-		sort.SliceStable(s, func(i, j int) bool {
-			if s[i].addrdata.ClassDate[class] == s[j].addrdata.ClassDate[class] {
-				return s[i].addrdata.Address < s[j].addrdata.Address
-			} else {
-				return s[i].addrdata.ClassDate[class] > s[j].addrdata.ClassDate[class]
-			}
-		})
-		for rank, kv := range s {
-			thisval, _ := thisclass[kv.normaddr]
-			thisval.RecencyRank = rank
-			thisclass[kv.normaddr] = thisval
-		}
-		for normaddr, addrdata := range thisclass {
-			addrdata.TotalRank = addrdata.FrequencyRank + addrdata.RecencyRank
-			thisclass[normaddr] = addrdata
-		}
-
-		classedData[class] = thisclass
+		classedData[class] = getClassRanks(classedData[class], class)
 	}
 	return classedData
 }
