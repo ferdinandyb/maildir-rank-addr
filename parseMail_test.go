050f1ce24045635b8c8f780959de527ab3a39ccc
Bence Ferdinandy <bence@ferdinandy.com>
2025-01-10 12:10:41 +0100

    chore: run gofumpt

--- parseMail_test.go
+++ parseMail_test.go
@@ -1,26 +1,24 @@
 package main
 
 import (
-	"testing"
 	"regexp"
+	"testing"
 )
 
-
 func TestAssignClass(t *testing.T) {
-
-	var useraddresses = []*regexp.Regexp {
+	useraddresses := []*regexp.Regexp{
 		regexp.MustCompile("foo@bar.com"),
 		regexp.MustCompile("name-.+@example.com"),
 	}
 
-	var tests = []struct {
-		testname string
-		in_field string
-		in_sender string
+	tests := []struct {
+		testname         string
+		in_field         string
+		in_sender        string
 		in_useraddresses []*regexp.Regexp
-		want  int
+		want             int
 	}{
-		{"No useraddresses", "something", "foo@bar.com", []*regexp.Regexp {}, 2},
+		{"No useraddresses", "something", "foo@bar.com", []*regexp.Regexp{}, 2},
 		{"Field is from", "from", "foo@bar.com", useraddresses, 0},
 		{"Explicit matching in to", "to", "foo@bar.com", useraddresses, 2},
 		{"Explicit matching in cc", "cc", "foo@bar.com", useraddresses, 1},
@@ -29,11 +27,11 @@ func TestAssignClass(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-	    t.Run(tt.testname, func(t *testing.T) {
-		ans := assignClass(tt.in_field, tt.in_sender, tt.in_useraddresses)
-		if ans != tt.want {
-		    t.Errorf("got %d, want %d", ans, tt.want)
-		}
-	    })
+		t.Run(tt.testname, func(t *testing.T) {
+			ans := assignClass(tt.in_field, tt.in_sender, tt.in_useraddresses)
+			if ans != tt.want {
+				t.Errorf("got %d, want %d", ans, tt.want)
+			}
+		})
 	}
 }
